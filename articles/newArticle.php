038d8e7034c7668dc2843243e77bed23de01d7d2
patrickkariukikabita <patrickkariuki13@gmail.com>
2024-07-31 13:25:28 +0300

    fixed tinymce token issue

--- articles/newArticle.php
+++ articles/newArticle.php
@@ -30,7 +30,7 @@ else{
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-  <script src="https://cdn.tiny.cloud/1/4u1uuliyidc8mn97a5t2dz2i4y6e2noa7q0edmzugnb1qicy/tinymce/6/tinymce.min.js"
+  <script src="https://cdn.tiny.cloud/1/[Your tinymce token]/tinymce/6/tinymce.min.js"
     referrerpolicy="origin"></script>
   <link rel="stylesheet" href="../menu/menu.css">
   <style>
